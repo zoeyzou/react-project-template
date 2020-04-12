48dd48b7bd0c65544b375600535897237e4697a5
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-12 16:43:20 +0200

    Move stores to its own folder

--- src/stores/counter/slice.ts
+++ src/stores/counter/slice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { AppThunk, RootState } from '../../app/store'
+
+import * as thunks from './thunks'
 
 interface CounterState {
   value: number
@@ -32,19 +33,6 @@ export const counterSlice = createSlice({
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount: number): AppThunk => (dispatch) => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount))
-  }, 1000)
-}
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value
+export const { incrementAsync } = thunks
 
 export default counterSlice.reducer
